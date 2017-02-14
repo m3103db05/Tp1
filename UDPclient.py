16310aefe35f8ab10448eb200b825679c701265a
m3103db05 <maiza.wu@gmail.com>
2017-02-14 18:26:26 +0100

    Add files via upload

---
+++ UDPclient.py
@@ -0,0 +1,17 @@
+from socket import *
+
+serverName = "localhost"
+
+serverPort = 12000
+
+clientSocket = socket(AF_INET,SOCK_DGRAM)
+
+message = input("sentence")
+
+clientSocket.sendto(message,(serverName, serverPort))
+
+modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+
+print modifiedMessage
+
+clientSocket.close()
